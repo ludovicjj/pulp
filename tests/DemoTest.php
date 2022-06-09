ebe203a02ef4dd04eadad9348c48659bd2b17f9d
ludovicjj <jahanlud@gmail.com>
2022-06-09 07:45:37 +0200

    init test

---
+++ tests/DemoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testAreWorking()
+    {
+        $this->assertEquals(1 + 1, 2);
+    }
+}
